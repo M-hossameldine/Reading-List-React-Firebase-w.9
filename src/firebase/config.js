56d9a61b917f1c4c0a5c197a111735b8decb2af0
Mohamed Hossam <m.hossameldine@gmail.com>
2022-05-18 20:02:37 +0200

    feat(firebase): add firestore configuration

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBB20__kNTngLfqdfqzIiV5-KQu0TWHyVs',
+  authDomain: 'reading-list-fb-w9.firebaseapp.com',
+  projectId: 'reading-list-fb-w9',
+  storageBucket: 'reading-list-fb-w9.appspot.com',
+  messagingSenderId: '175376024946',
+  appId: '1:175376024946:web:5a93b22bed05f6028089eb',
+};
+
+// init firestore app
+initializeApp(firebaseConfig);
+
+// init firestore
+const db = getFirestore();
+
+export { db };
