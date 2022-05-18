d87b2273f06d03222bb09110f6e3629066ff5043
Mohamed Hossam <m.hossameldine@gmail.com>
2022-05-18 21:48:41 +0200

    feat(firestore): :sparkles: delete book on click

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,16 +1,23 @@
-export default function BookList({ books }) {
+// firebase
+import { db } from '../firebase/config';
+import { doc, deleteDoc } from 'firebase/firestore';
 
-  const handleClick = async (id) => {
-    console.log(id)
-  }
+export default function BookList({ books }) {
+  const handleDeleteClick = async (id) => {
+    const docRef = doc(db, 'books', id);
+
+    await deleteDoc(docRef);
+  };
 
   return (
-    <div className="book-list">
+    <div className='book-list'>
       <ul>
-        {books.map(book => (
-          <li key={book.id} onClick={() => handleClick(book.id)}>{book.title}</li>
+        {books.map((book) => (
+          <li key={book.id} onClick={() => handleDeleteClick(book.id)}>
+            {book.title}
+          </li>
         ))}
       </ul>
     </div>
-  )
+  );
 }
