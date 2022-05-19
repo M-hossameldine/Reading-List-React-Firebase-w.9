a97f08b4e0d6947c091f948f667fbb4d6e1c5966
Mohamed Hossam <m.hossameldine@gmail.com>
2022-05-19 17:08:21 +0200

    fix: :hammer: show nav links conditionally

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 export default function Navbar() {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <nav>
       <h1>My Reading List</h1>
       <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-        <li>
-          <Link to='/signup'>Signup</Link>
-        </li>
-        <li>
-          <button style={{ fontSize: '15px' }} onClick={logout}>
-            Logout
-          </button>
-        </li>
+        {user && (
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+        )}
+        {!user && (
+          <>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+            <li>
+              <Link to='/signup'>Signup</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <li>
+            <button style={{ fontSize: '15px' }} onClick={logout}>
+              Logout
+            </button>
+          </li>
+        )}
       </ul>
     </nav>
   );
